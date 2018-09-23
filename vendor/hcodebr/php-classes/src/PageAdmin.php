2d953c0d5990b0c5c8b43d36017a55117fe631fb
wisleysiqueira <wisleysiqueira@hotmail.com>
2018-09-22 22:47:56 -0300

    criando page admin e tpl admin

---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,8 @@
+<?php  
+	namespace Hcode;
+	class PageAdmin extends Page{
+		public function __construct($opts = array(), $tpl_dir = "/ecommerce/views/admin/"){
+			parent::__construct($opts, $tpl_dir);
+		}
+	}
+?>
