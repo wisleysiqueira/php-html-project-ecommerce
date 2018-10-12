0a17b38dd01ebcdcb0c675fede9dd1209da5312d
wisleysiqueira <wisleysiqueira@hotmail.com>
2018-10-12 00:37:34 -0300

    Detalhes do Produto

--- site.php
+++ site.php
@@ -30,4 +30,14 @@ $app->get("/categories/:idcategory", function($idcategory){
 		'pages'=>$pages
 	]);
 });
+
+$app->get("/products/:desurl", function($desurl){
+	$product = new Product();
+	$product->getFromURL($desurl);
+	$page = new Page();
+	$page->setTpl("product-detail", [
+		'product'=>$product->getValues(),
+		'categories'=>$product->getCategories()
+	]);
+});
 ?>
