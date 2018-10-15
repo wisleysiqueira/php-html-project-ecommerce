ac2273d0685e2390d7588062364c4435776abcf0
wisleysiqueira <wisleysiqueira@hotmail.com>
2018-10-15 02:35:40 -0300

    Carrinho Calculo de Frete

--- site.php
+++ site.php
@@ -47,7 +47,8 @@ $app->get("/cart", function(){
 	$page = new Page();
 	$page->setTpl("cart", [
 		'cart'=>$cart->getValues(),
-		'products'=>$cart->getProducts()
+		'products'=>$cart->getProducts(),
+		'error'=>Cart::getMsgError()
 	]);
 });
 
@@ -77,6 +78,13 @@ $app->get("/cart/:idproduct/remove", function($idproduct){
 	$product->get((int)$idproduct);
 	$cart = Cart::getFromSession();
 	$cart->removeProduct($product, true);
+	header("Location: /ecommerce/cart");
+	exit;
+});
+
+$app->post("/cart/freight", function(){
+	$cart = Cart::getFromSession();
+	$cart->setFreight($_POST['zipcode']);
 	header("Location: /ecommerce/cart");
 	exit;
 });
