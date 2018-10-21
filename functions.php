13139fff3f6cfd8c59cda4b352964c6712b970d5
wisleysiqueira <wisleysiqueira@hotmail.com>
2018-10-21 12:47:32 -0300

    Esqueceu a Senha Site

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php  
 use \Hcode\Model\User;
-	function formatPrice(float $vlprice)
+	function formatPrice($vlprice)
         {
+            if(!$vlprice > 0) $vlprice = 0;
             return number_format($vlprice, 2, ",", "."); 
         }
 
